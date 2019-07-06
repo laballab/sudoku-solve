e4f0e1a353cba4657b40821a18ffa10b098057dc
Luis Ballabeni <lballabe@cisco.com>
2019-07-06 13:22:45 -0400

    random stuff.. forgot

--- src/sudoku_solve.c
+++ src/sudoku_solve.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-
-void show_grid(char *grid_data) {
-  printf("%s", grid_data);
-}
+#include "../include/sudoku_solve.h"
 
 int main(int argc, char *argv[]) {
 	printf("*==[Sudoku Solver]==*\n");
   if (argc == 2) {
     show_grid(argv[1]);
+  } else {
+	  printf("Input a puzzle!\n");
   }
 
 	return 0;
 }
+
+void show_grid(char *grid_data) {
+  // TODO function to render the game grid 
+  printf("%s\n", grid_data);
+}
+
+
 
