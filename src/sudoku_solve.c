f7a7a871608df66461b7ba21316512ce9d4acdf8
Patricia Ray <patrray@cisco.com>
2019-07-14 13:06:24 -0400

    Deserializing puzzle input

--- src/sudoku_solve.c
+++ src/sudoku_solve.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
-#include "../include/sudoku_solve.h"
+#include <stdlib.h>
+#include <string.h>
 
 struct sudoku_board {
-  int count_remaining[9] ;
-  int grid[9][9] ;
-} board;
+  int count_remaining[9];
+  int grid[9][9];
+};
 
-int main(int argc, char *argv[]) {
-	printf("*==[Sudoku Solver]==*\n");
-  if (argc == 2) {
-    show_grid(argv[1]);
-  } else {
-	  printf("Input a puzzle!\n");
+void deserialize_puzzle(char *puzzle_file) {
+  struct sudoku_board game_board = { .count_remaining = { 9 }, .grid = { {0, 0}, {0, 0}  } };
+  FILE *fp;
+  char buff[128];
+
+  fp = fopen(puzzle_file, "r");
+  fscanf(fp, "%s", buff);
+  fscanf(fp, "%s", buff);
+  printf("1 : %s\n", buff);
+  
+  for(int row = 0; row < 9; row++) {
+    for(int col = 0; col < 9; col++) {
+      if(buff[row * 9 + col] != '.') {
+        int value = atoi(&buff[row * 9 + col]);
+        game_board.grid[col][row] = value;
+        game_board.count_remaining[value]--;
+      }
+    }
   }
-
-	return 0;
+  printf("%s\n", game_board.grid);
 }
 
 void show_grid(char *grid_data) {
@@ -22,5 +34,13 @@ void show_grid(char *grid_data) {
   printf("%s\n", grid_data);
 }
 
+int main(int argc, char *argv[]) {
+	printf("*==[Sudoku Solver]==*\n");
+  if (argc == 2) {
+    deserialize_puzzle(argv[1]);
+  } else {
+	  printf("Input a puzzle!\n");
+  }
 
-
+	return 0;
+}
